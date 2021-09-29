42edb8554a1d6b3f2e5867648c0dfb0614b6a8dd
OscarSaladillo <91348351+OscarSaladillo@users.noreply.github.com>
2021-09-29 12:54:05 +0200

    jeje

---
+++ claseBucle.dart
@@ -0,0 +1,38 @@
+
+import 'dart:io';
+
+class nIterator {
+  int _nEle;
+  Set<int> set = {};
+
+  nIterator(int numE) {
+    _nEle = numE;
+  }
+
+  nIterator.withList(int numE) {
+    _nEle = numE;
+    for(int i=0;i<=_nEle;i++) {
+      set.add(i);
+    }
+  }
+
+  void iterate() {
+    for(int i=0;i<=_nEle;i++) {
+      print(i);
+    }
+  }
+
+  void iterate2() {
+    for(int i in set) {
+      print(i);
+    }
+  }
+
+}
+
+void main() {
+  nIterator iterador, iterador2;
+  print("Introduce el numero de elementos");
+  iterador = nIterator(int.parse(stdin.readLineSync().toString()))..iterate();
+  iterador2 = nIterator.withList(int.parse(stdin.readLineSync().toString()))..iterate();
+}
